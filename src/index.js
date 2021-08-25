c5dbd7b6ed5efb1f88a2398ef6019f5bbecde360
Alona-Isko <79259446+Alona-Isko@users.noreply.github.com>
2021-08-25 22:54:35 +0200

    refs, value for input, fetch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import './sass/main.scss';
+import './js/fetchCountries.js';
 
 import { alert, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
-  import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
  
   defaultModules.set(PNotifyMobile, {});
  
@@ -15,3 +16,36 @@ debounce(test, 2000)
 function test () {
   console.log('object')
 }
+
+
+const refs = {
+  form: document.querySelector('#form'),
+  input: document.querySelector('#search'),
+  container: document.querySelector('.container')
+}
+
+// function fetchCountries(searchQuery) {
+//   return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+//     .then(response => response.json())
+  
+//     .catch(error => console.log(error))
+// }
+
+fetch('https://restcountries.eu/rest/v2/all').then(response => {
+  return response.json();
+}).then(country => {
+  console.log(country);
+})
+  .catch(error => {
+    console.log(error);
+  });
+
+  
+const onSearch = (ev) => {
+  ev.preventDefault();
+  const value = refs.input.value
+
+  console.log(value);
+}
+
+refs.form.addEventListener('submit', onSearch);
